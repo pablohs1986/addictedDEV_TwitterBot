0f4ae2ab3071b7e2b78e5b6d8038c928d703aa0d
pablohs1986 <pabloherrero1986@gmail.com>
2020-09-05 17:40:38 +0200

    Added more statuses and hashtags

--- data/hashtags.js
+++ data/hashtags.js
@@ -3,5 +3,5 @@ module.exports = [
     '#nodejs', '#nodeJS', '#100DaysOfCode', '#100daysofcode', '#100DOC', '#100doc', 
     '#stackoverflow', '#StackOverFlow', '#angular', '#Angular', '#VSCode', '#vscode', 
     '#Netbeans', '#netbeans', '#developer', '#Developer', '#Development', '#development', 
-    '#sql', '#SQL'
+    '#sql', '#SQL', '#code', '#coder', '#coding', '#loveCode'
 ]
