c3e4341055ef8415231304c98c70a9963952d0cc
pablohs1986 <pabloherrero1986@gmail.com>
2020-09-05 14:24:39 +0200

    Separating data from code (add data folder)

---
+++ data/hashtags.js
@@ -0,0 +1,7 @@
+module.exports = [
+    '#java', '#Java', '#js', '#javascript', '#JavaScript', '#python', '#Python', 
+    '#nodejs', '#nodeJS', '#100DaysOfCode', '#100daysofcode', '#100DOC', '#100doc', 
+    '#stackoverflow', '#StackOverFlow', '#angular', '#Angular', '#VSCode', '#vscode', 
+    '#Netbeans', '#netbeans', '#developer', '#Developer', '#Development', '#development', 
+    '#sql', '#SQL'
+]
